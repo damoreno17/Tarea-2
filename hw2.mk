5b4ef14caf84b67f7a4525f2d6d3716987817253
Diego <da.moreno17@uniandes.edu.co>
2019-05-11 21:32:27 -0700

    archivo make

---
+++ hw2.mk
@@ -0,0 +1,15 @@
+Señales.pdf : Fourier.py signal.dat signalSuma.dat
+	python Fourier.py
+
+Fourie.pdf : Fourier.py signal.dat signalSuma.dat
+	python Fourier.py
+    
+    
+fourie_temblor.pdf : Fourier.py temblor.txt
+	python Fourier.py 
+
+espectro_temblor.pdf : Fourier.py temblor.txt
+python Fourier.py 
+
+espectro_signal.pdf : Fourier.py signal.dat signalSuma.dat
+python Fourier.py
